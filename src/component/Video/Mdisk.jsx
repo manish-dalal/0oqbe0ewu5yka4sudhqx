cada08874c838363c3e8c267bd453b0cf31befdf
Manish Dalal <manish.dalal@moonshinetechnology.com>
2022-05-10 19:29:46 +0530

    changes

--- src/component/Video/Mdisk.jsx
+++ src/component/Video/Mdisk.jsx
@@ -3,7 +3,7 @@ import './mdisk.css';
 import Report from '../Report';
 import Play from '../MdiskInfo/Play';
 import MdiskDetail from '../MdiskInfo/MdiskDetail';
-import { transformMdiskGet } from '../../utils/mdisk';
+import { transformMdiskGet, isIOS } from '../../utils/mdisk';
 
 export default function Mdisk({
   videoId,
@@ -14,9 +14,9 @@ export default function Mdisk({
 }) {
   useEffect(() => {
     if (videoId) {
-      let params = {
-        url: `https://diskuploader.entertainvideo.com/v1/file/cdnurl?param=${videoId}`,
-      };
+      let apiUrl = `https://diskuploader.entertainvideo.com/v1/file/cdnurl?param=${videoId}`;
+      if (isIOS) apiUrl = apiUrl + '&sys=ios';
+      let params = { url: apiUrl };
       const url = new URL(`https://diskuploader.glitch.me/api/msc/get`);
       url.search = new URLSearchParams(params);
       fetch(url.href)
@@ -36,4 +36,20 @@ export default function Mdisk({
       <MdiskDetail videoData={videoData} />
     </div>
   );
+}
+
+{
+  // height={3660}
+  /* <Iframe
+        id='mdisk-element'
+        src={`https://mdisk.me/convertor/14x9/${videoId}`}
+        height={topHeight}
+        width={Math.min(480, width)}
+        title='Mdisk'
+        scrolling='no'
+        frameBorder={0}
+        allowFullScreen={true}
+        onInferredClick={onClick}
+        onLoad={onLoad}
+      /> */
 }
